96fc752765b7a9fe2ee9f742f936b41ca48924cf
iron_aidan <github@ironaidan.neomailbox.ch>
2018-09-07 11:25:07 -0700

    Return an emotion based on picture

--- js/app.js
+++ js/app.js
@@ -6,45 +6,64 @@ var config = {
   databaseURL: "https://project-one-3e89e.firebaseio.com",
   storageBucket: "project-one-3e89e.appspot.com",
 };
+
 // I needed to make these variables global so we can access the url outside of the function later
 var downloadURL;
 var file;
 var storageRef;
 var thisRef;
+var fileUploaded = false;
+var urlRetrieved = false;
 
 firebase.initializeApp(config);
 
 // Function to save file. Called when button is clicked
 function uploadFile() {
-  file = $('#files').get(0).files[0];
-  storageRef = firebase.storage().ref();
-  
-  // Dynamically set reference to the file name
-  thisRef = storageRef.child(file.name);
+  // if (!file) {
+    file = $('#files').get(0).files[0];
+    storageRef = firebase.storage().ref();
+    thisRef = storageRef.child(file.name);
 
-  // Put request upload file to Firebase storage
-  thisRef.put(file).then(function(snapshot) {
-    console.log('Uploaded file!');
-  });
-
-  // I'm using a timer to fix an error where the url request is happening before the file has uploaded, and therefore, there is no url. 
-  // There is a better way to do this, but I don't know how yet
-  setTimeout(function() {
-
-    // Get request to get URL for uploaded file
-    thisRef.getDownloadURL().then(function(url) {
-      console.log(url);
-      downloadURL = url;
-    })
-
-    // Ajax call using get request url as source for detection
-    $.ajax({
-      url: 'https://apis.paralleldots.com/v3/facial_emotion?api_key=cnzJZhio5ZfrFxoo23hDZlpsK34EARxBnXCKDkFuqAQ&url=' + downloadURL,
-      method: 'POST'
-    }).then(function(response) {
-      console.log(response);
+    // Upload file to Firebase storage
+    thisRef.put(file).then(function(snapshot) {
+      fileUploaded = !fileUploaded;
+      console.log("File Uploaded");
+      retrieveUrl();
+    }).catch(err => {
+      console.log(err);
     });
-  }, 2000);
+  // }
+
+  // Retrieve URL for uploaded file
+  function retrieveUrl() {
+    if (fileUploaded) {
+      thisRef.getDownloadURL().then(function(url) {
+      downloadURL = url;
+      fileUploaded = !fileUploaded;
+      urlRetrieved = !urlRetrieved;
+      console.log("URL Retrieved");
+      emotionDetect();
+      }).catch(err => {
+        console.log(err);
+      });
+    }
+  }
+
+  // Ajax call using url as source for detection
+  function emotionDetect() {
+    if (urlRetrieved) {
+      $.ajax({
+        url: 'https://apis.paralleldots.com/v3/facial_emotion?api_key=cnzJZhio5ZfrFxoo23hDZlpsK34EARxBnXCKDkFuqAQ&url=' + downloadURL,
+        method: 'POST'
+      }).then(function(response) {
+        console.log(response.facial_emotion[0].tag);
+        return response.facial_emotion[0].tag;
+      }).catch(err => {
+        console.log(err);
+      });
+      urlRetrieved = !urlRetrieved;
+    }
+  }
 
 }
 
