e8440935cb5e912f0590fab857c0d5bb2c92083b
iron_aidan <github@ironaidan.neomailbox.ch>
2018-09-07 19:38:31 -0700

    Integrate mood and music

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ var storageRef;
 var thisRef;
 var fileUploaded = false;
 var urlRetrieved = false;
+var mood;
 
 firebase.initializeApp(config);
 
@@ -57,7 +58,8 @@ function uploadFile() {
         method: 'POST'
       }).then(function(response) {
         console.log(response.facial_emotion[0].tag);
-        return response.facial_emotion[0].tag;
+        mood = response.facial_emotion[0].tag
+        retrieveSong();
       }).catch(err => {
         console.log(err);
       });
@@ -65,5 +67,71 @@ function uploadFile() {
     }
   }
 
-}
+  const tracksTemplateSource = document.getElementById('tracks-template').innerHTML;
 
+  // Not sure if these two lines are needed because the variables are never read
+  const tracksTemplate = Handlebars.compile(tracksTemplateSource);
+  const $tracks = $('#tracks-container');
+
+  var apikey = "apikey=NDU0ZjU2ZWMtMjVhMS00NmNlLWI3NWItYTdlNTc5ODdkMzNk";
+  var trackID;
+  
+  // Match mood to appropriate music choice
+  function retrieveSong() {
+    switch (mood) {
+      case 'Angry':
+        trackID = "tra.128493454";
+        break;
+      case 'Fear':
+        trackID = "tra.268797739";
+        break;
+      case 'Neutral':
+        trackID = "tra.305200130";
+        break;
+      case 'Surprise':
+        trackID = "tra.41390755";
+        console.log('You are surprised');
+        break;
+      case 'Sad':
+        trackID = "tra.2732140";
+        break;
+      case 'Happy':
+        trackID = "tra.257617960";
+        break;
+      case 'Disgust':
+        trackID = "tra.1341825";
+        break;
+      default:
+        console.log('You are a robot with no emotions');
+        break;
+    }
+    
+    // Update queryURL with appropriate trackID
+    var queryURL = "https://api.napster.com/v2.2/tracks/" + trackID + "?" + apikey;
+  
+    // Ajax that shit
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      var database = response.tracks;
+      console.log(database)
+  
+      var a = $("<audio controls>");
+      var p = $('<p class="artistInfo">');
+      var c = $('<p class="artistInfo">');
+  
+      p.html(database[0].name + " by " + database[0].artistName);
+      $(".preview").append(p);
+  
+      a.attr("src", database[0].previewURL);
+      $(".preview").append(a);
+  
+      c.html(database[0].albumName + " Album");
+      $(".preview").append(c);
+
+    });
+
+  }
+  
+}
