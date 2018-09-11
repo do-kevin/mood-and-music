511bc38273175083243b85ef95f8bcc0f390f564
Kevin Do <do.kevin.vo@gmail.com>
2018-09-11 13:11:25 -0700

    Cleaned up webcam js; WIP b64 to Blob

--- js/app.js
+++ js/app.js
@@ -1,60 +1,79 @@
 //=========================================== Webcam (WebRTC)====================================
+var video = document.querySelector("#video"),
+  canvas = document.querySelector("#canvas"),
+  context = canvas.getContext("2d"),
+  photo = document.querySelector("#photo");
 
-function webcam() {
-  var video = document.querySelector("#video"),
-    canvas = document.querySelector("#canvas"),
-    context = canvas.getContext("2d"),
-    photo = document.querySelector("#photo");
+var constraints = {
+  audio: false,
+  video: {
+    width: 320,
+    height: 240
+  }
+};
 
-  var constraints = {
-    audio: false,
-    video: {
-      width: 320,
-      height: 240
-    }
-  };
-
-  navigator.mediaDevices.getUserMedia(constraints)
-    .then(function(mediaStream) {
-      video.srcObject = mediaStream;
-      video.onloadedmetadata = function(e) {
-        video.play();
-      };
-    }).catch(function(err) {
-      // Old browser support?
-      navigator.getMedia = navigator.getUserMedia ||
+navigator.mediaDevices
+  .getUserMedia(constraints)
+  .then(function(mediaStream) {
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = function(e) {
+      video.play();
+    };
+  })
+  .catch(function(err) {
+    // Old browser support?
+    navigator.getMedia =
+      navigator.getUserMedia ||
       navigator.webkitGetUserMedia ||
       navigator.mozGetUserMedia || // Mozilla
       navigator.msGetUsermedia; // Microsoft IE
-      navigator.getMedia({
+    navigator.getMedia(
+      {
         video: true,
         audio: false // No need to capture audio
-      }, function(stream) {
+      },
+      function(stream) {
         video.srcObject = stream;
         video.play();
-      }, function(err) {
+      },
+      function(err) {
         console.log(err);
-      });
-    });
-
-    document.querySelector("#capture").addEventListener("click", function () {
-      //What I want to draw on
-      // (video, IDK, IDK, width, height)
-      context.drawImage(video, 0, 0, 320, 240);
-
-      // Grab from the canvas and placing into the photo src, which is the link and where the picture is saved.
-      // The src of the file is transferred to "data:image/png;base64[picture's code]"
-      // For example, take a picture. Right-click on the pic and you can see its address
-      //              bar when you open it in a new tab; however, you cannot copy its image address.
-      // The ".toDataURL" spits out base64 code which will be attached to the source
-      photo.setAttribute("src", canvas.toDataURL("image/jpeg"));
+      }
+    );
   });
 
-}
+document.querySelector("#capture").addEventListener("click", function() {
+  console.log("hit");
+  //What I want to draw on
+  // (video, IDK, IDK, width, height)
+  context.drawImage(video, 0, 0, 320, 240);
 
-webcam();
+  // Grab from the canvas and placing into the photo src, which is the link and where the picture is saved.
+  // The src of the file is transferred to "data:image/png;base64[picture's code]"
+  // For example, take a picture. Right-click on the pic and you can see its address
+  //              bar when you open it in a new tab; however, you cannot copy its image address.
+  // The ".toDataURL" spits out base64 code which will be attached to the source
+  photo.setAttribute("src", canvas.toDataURL("image/jpeg", 1.0));
+  console.log(photo);   
+  var b64Data = canvas.toDataURL("image/jpeg", 1.0);
 
+  function b64toBlob(b64Data) {
+    // console.log(b64Data);
+    var byteString = atob(b64Data.split(",")[1]);
+    // console.log(byteString);
+    var ab = new ArrayBuffer(byteString.length);
+    // console.log(ab);
+    var ia = new Uint8Array(ab);
+    console.log(ia);
 
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: "image/jpeg" });
+  }
+  // console.log(b64toBlob(b64Data));
+  
+});
 
 //==============================================Initialize Firebase==================================================
 var config = {
@@ -81,58 +100,79 @@ firebase.initializeApp(config);
 
 // Function to save file. Called when button is clicked
 function uploadFile() {
+  var blob = b64toBlob(b64Data);
   file = $("#files").get(0).files[0];
-    if (file !== undefined) {
-      storageRef = firebase.storage().ref();
-      thisRef = storageRef.child(file.name);
+  // file = blob;
+  if (file !== undefined) {
+    storageRef = firebase.storage().ref();
+    thisRef = storageRef.child(file.name);
 
-      // Upload file to Firebase storage
-      thisRef.put(file).then(function(snapshot) {
+    // Upload file to Firebase storage
+    thisRef
+      .put(file)
+      .then(function(snapshot) {
         fileUploaded = !fileUploaded;
         console.log("File Uploaded");
         retrieveUrl();
-      }).catch(err => {
+      })
+      .catch(err => {
         console.log(err);
       });
-    }
+  }
 
   // Retrieve URL for uploaded file
   function retrieveUrl() {
     if (fileUploaded) {
-      thisRef.getDownloadURL().then(function(url) {
-        downloadURL = url;
-        fileUploaded = !fileUploaded;
-        urlRetrieved = !urlRetrieved;
-        console.log("URL Retrieved");
-        emotionDetect();
-      }).catch(err => {
-        console.log(err);
-      });
+      thisRef
+        .getDownloadURL()
+        .then(function(url) {
+          downloadURL = url;
+          fileUploaded = !fileUploaded;
+          urlRetrieved = !urlRetrieved;
+          console.log("URL Retrieved");
+          emotionDetect();
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 
-//======================================== ParallelDots ===========================================
+  //======================================== ParallelDots ===========================================
   // Ajax call using url as source for detection
   function emotionDetect() {
     if (urlRetrieved) {
       var dotsAPIkey = "Cr6V9f2rl8RJDzQQp1ZFRwosg73K8k0MOcOCf4d119E";
       $.ajax({
-        url: "https://apis.paralleldots.com/v3/facial_emotion?api_key=" + dotsAPIkey +"&url=" + downloadURL,
+        url:
+          "https://apis.paralleldots.com/v3/facial_emotion?api_key=" +
+          dotsAPIkey +
+          "&url=" +
+          downloadURL,
         method: "POST"
       }).then(function(response) {
         console.log(response.code);
-        if ((response.code >= 400) ||
-        (response.output === "No face detected.")) {
+        if (response.code >= 400 || response.output === "No face detected.") {
           console.log("You are probably a robot with no emotions");
-          $(".preview").empty().hide("scale");
+          $(".preview")
+            .empty()
+            .hide("scale");
           let e = $('<br><p class="errBox white-text">');
-          $(".preview").append(e).show("scale", 1050);
-          e.html("You're probably an emotionless robot. Please upload another image file with a headshot and sufficient lighting.");
+          $(".preview")
+            .append(e)
+            .show("scale", 1050);
+          e.html(
+            "You're probably an emotionless robot. Please upload another image file with a headshot and sufficient lighting."
+          );
         } else if (response.code === 403) {
           dotsAPIkey = "n3yLuB3RxgDcj5DYAxaxtqxbNqtszhif3dvP4wtrtYE";
-          $(".preview").empty().hide("scale");
+          $(".preview")
+            .empty()
+            .hide("scale");
           let e = $('<br><p class="errBox white-text">');
-          $(".preview").append(e).show("scale", 1050);
+          $(".preview")
+            .append(e)
+            .show("scale", 1050);
           e.html("API key changed, please try again.");
         } else {
           console.log(response.facial_emotion[0].tag);
@@ -144,7 +184,7 @@ function uploadFile() {
     }
   }
 
-//======================================= Napster ==============================================
+  //======================================= Napster ==============================================
   var apikey = "apikey=NDU0ZjU2ZWMtMjVhMS00NmNlLWI3NWItYTdlNTc5ODdkMzNk";
   var trackID;
 
@@ -178,7 +218,8 @@ function uploadFile() {
     }
 
     // Update queryURL with appropriate trackID
-    var queryURL = "https://api.napster.com/v2.2/tracks/" + trackID + "?" + apikey;
+    var queryURL =
+      "https://api.napster.com/v2.2/tracks/" + trackID + "?" + apikey;
 
     $.ajax({
       url: queryURL,
@@ -192,16 +233,25 @@ function uploadFile() {
       var c = $('<p class="artistInfo center-align white-text">');
 
       // Empty audio controls before every request so we don't get duplicates
-      $(".preview").empty().hide("scale");
+      $(".preview")
+        .empty()
+        .hide("scale");
 
       p.html(database[0].name + " by " + database[0].artistName);
-      $(".preview").append(p).show("scale", 1050);
+      $(".preview")
+        .append(p)
+        .show("scale", 1050);
 
       a.attr("src", database[0].previewURL);
-      $(".preview").attr("id", "song").append(a).show("scale", 1060);
+      $(".preview")
+        .attr("id", "song")
+        .append(a)
+        .show("scale", 1060);
 
       c.html(database[0].albumName + " Album").show("scale", 1200);
-      $(".preview").append(c).show("scale", 1100);
+      $(".preview")
+        .append(c)
+        .show("scale", 1100);
 
       localStorage.clear();
 
@@ -213,18 +263,35 @@ function uploadFile() {
   }
 }
 
-if ((localStorage.getItem("song") !== null) ||
-  (localStorage.getItem("artist") !== null) ||
-  (localStorage.getItem("url") !== null) ||
-  (localStorage.getItem("album") !== null)) {
+if (
+  localStorage.getItem("song") !== null ||
+  localStorage.getItem("artist") !== null ||
+  localStorage.getItem("url") !== null ||
+  localStorage.getItem("album") !== null
+) {
+  var p = $("<p>")
+    .addClass("artistInfo center-align white-text")
+    .html(
+      localStorage.getItem("song") + " by " + localStorage.getItem("artist")
+    );
+  $(".preview")
+    .append(p)
+    .show("scale", 1050);
 
-  var p = $("<p>").addClass("artistInfo center-align white-text").html(localStorage.getItem("song") + " by " + localStorage.getItem("artist"));
-  $(".preview").append(p).show("scale", 1050);
+  var a = $("<audio controls autoplay>").attr(
+    "src",
+    localStorage.getItem("url")
+  );
+  $(".preview")
+    .attr("id", "song")
+    .append(a)
+    .show("scale", 1060);
 
-  var a = $("<audio controls autoplay>").attr("src", localStorage.getItem("url"));
-  $(".preview").attr("id", "song").append(a).show("scale", 1060);
-
-  var c = $("<p>").addClass("artistInfo center-align white-text").html(localStorage.getItem("album") + " Album").show("scale", 1200);
-  $(".preview").append(c).show("scale", 1100);
-
+  var c = $("<p>")
+    .addClass("artistInfo center-align white-text")
+    .html(localStorage.getItem("album") + " Album")
+    .show("scale", 1200);
+  $(".preview")
+    .append(c)
+    .show("scale", 1100);
 }
